99aebd387b16f8879e8bb4ee06f32c854aed588d
paabloomaartinez <pablo121002@gmail.com>
2023-04-16 21:06:01 +0200

    register conectado

--- Lab5-Django/Lab5_Django/urls.py
+++ Lab5-Django/Lab5_Django/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 """
 
 # Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
+import usuario.views
 
 urlpatterns = [
-    # Uncomment the next line to enable the admin:
-    #path('admin/', admin.site.urls)
+    #url(r'^admin/', admin.site.urls),
+    url(r'^$', usuario.views.register_request, name='register')
 ]
