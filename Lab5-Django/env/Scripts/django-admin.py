27c84ee9891fc86693c505ced9754c42a0a7f51b
jonor <jonor@LAPTOP-BOCES9J9>
2023-04-16 15:44:26 +0200

    Add project files.

---
+++ Lab5-Django/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jonor\source\repos\Lab5-Django\Lab5-Django\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
