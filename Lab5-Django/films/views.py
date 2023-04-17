e7778f13fe78e9d968c87b8b87f597ab1dd74483
paabloomaartinez <pablo121002@gmail.com>
2023-04-17 23:21:14 +0200

    Ver Peliculas hecho

--- Lab5-Django/films/views.py
+++ Lab5-Django/films/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout, authenticate, login as auth_login
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Filma
 
 def logout(request):
     auth_logout(request)
@@ -9,7 +11,19 @@ def home(request):
     return render(request, 'films/home.html')
 
 def verPelis(request):
-    return render(request, 'films/verPelis.html')
+    objetos = Filma.objects.all()
+    paginator = Paginator(objetos, 4)
+    
+    page = request.GET.get('page')
+    try:
+        objetos_por_pagina = paginator.page(page)
+    except PageNotAnInteger:
+        objetos_por_pagina = paginator.page(1)
+    except EmptyPage:
+        objetos_por_pagina = paginator.page(paginator.num_pages)
+
+    return render(request, 'films/verPelis.html', {'objetos_por_pagina': objetos_por_pagina})
+
 
 def votar(request):
     return render(request, 'films/votar.html')
