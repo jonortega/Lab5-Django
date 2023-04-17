5e864f72f089f42c37c5c3260dab2c2f6dbab231
RAUL SILVA ALONSO <rsilva003@ikasle.ehu.eus>
2023-04-17 17:29:24 +0200

    Login hecho

--- Lab5-Django/Lab5_Django/urls.py
+++ Lab5-Django/Lab5_Django/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', usuario.views.home, name='home'),
     url(r'^/register', usuario.views.register_request, name='register'),
+    url(r'^/login', usuario.views.login, name='login'),
+
 ]
--- Lab5-Django/usuario/views.py
+++ Lab5-Django/usuario/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def register_request(request):
 	if request.method == "POST":
@@ -17,3 +18,21 @@ def register_request(request):
 
 def home(request):
     return render(request, 'usuario/home.html')
+
+def login(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="usuario/login.html", context={"login_form":form})
