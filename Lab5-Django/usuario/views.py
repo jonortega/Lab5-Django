a66bdb5ef72f28f4d4d191ad22e1b2ba1b878806
RAUL SILVA ALONSO <rsilva003@ikasle.ehu.eus>
2023-04-17 18:15:00 +0200

    Login y register tirando

--- Lab5-Django/usuario/views.py
+++ Lab5-Django/usuario/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -9,9 +9,10 @@ def register_request(request):
 		form = NewUserForm(request.POST)
 		if form.is_valid():
 			user = form.save()
-			login(request, user)
+			auth_login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
+			#VA MAL
+			return render(request=request, template_name="usuario/login.html", context={"login_form":form})
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="usuario/register.html", context={"register_form":form})
@@ -27,9 +28,9 @@ def login(request):
 			password = form.cleaned_data.get('password')
 			user = authenticate(username=username, password=password)
 			if user is not None:
-				login(request, user)
+				auth_login(request, user)
 				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:homepage")
+				return render(request, 'usuario/home.html')
 			else:
 				messages.error(request,"Invalid username or password.")
 		else:
